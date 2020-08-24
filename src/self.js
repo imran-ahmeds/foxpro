c91cf7bdb07f260082c489ce28e5ae70af20bdc6
Imran Ahmed Siddiqui <37952664+imran-ahmeds@users.noreply.github.com>
2020-08-24 15:35:46 +0500

    corrected

---
+++ src/self.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Self(props){
+    return(
+        <div>
+        <h1>This app is created with {props.langName}</h1>
+    </div>
+    );
+ 
+}
+export default Self;
